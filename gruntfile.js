a692d1d9b2a75312acc01a2e220a724875ebd704
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-02-10 13:03:21 -0500

    Minimal grunt file (no extra files)

--- gruntfile.js
+++ gruntfile.js
@@ -2,14 +2,51 @@ module.exports = function (grunt) {
     "use strict";
     /*eslint-env node*/
 
+    // This will measure the time spent in each task
     require("time-grunt")(grunt);
 
-    // Since the tasks are split using load-grunt-config, I need a global object containing the configuration
-    global.configuration = {
-        pkg: grunt.file.readJSON("./package.json")
-    };
+    // This will configure each task individually
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
 
-    require("load-grunt-config")(grunt);
-    grunt.task.loadTasks("grunt/tasks");
+        eslint: {
+            target: [
+                "grunt/**/*.js",
+                "src/**/*.js",
+                "test/**/*.js"
+            ]
+        },
+
+        mochaTest: {
+            source: {
+                options: {
+                    reporter: "spec",
+                    quiet: false,
+                    require: [
+                        function () {
+                            global.assert = require("assert");
+                        },
+                        "test/host/node.js"
+                    ]
+                },
+                src: "test/*.js"
+            }
+        }
+
+    });
+
+    // Load the packages that adds features to grunt
+    [
+        "grunt-eslint",
+        "grunt-mocha-test"
+    ].forEach(function (packageName) {
+        grunt.loadNpmTasks(packageName);
+    });
+
+    // Default task
+    grunt.registerTask("default", [
+        "eslint",
+        "mochaTest"
+    ]);
 
 };
