7fe9b960c0e1e4e9b9459f4a37fd5446cf56cd4a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-02-11 00:16:13 -0500

    Showing use cases

--- ref/test/test.js
+++ ref/test/test.js
@@ -9,4 +9,28 @@ describe("Test", function () {
         assert(false);
     });
 
+    it("supports asynchronism", function (done) {
+        var a = 0;
+        setTimeout(function () {
+            assert(a === 1);
+            done();
+        }, 100);
+        a = 1;
+    });
+
+    it("fails asynchronously", function (done) {
+        var a = 0;
+        setTimeout(function () {
+            assert(a === 0);
+            done();
+        }, 100);
+        a = 1;
+    });
+
+    it("fails if done is called with a parameter", function (done) {
+        setTimeout(function () {
+            done("error");
+        }, 100);
+    });
+
 });
