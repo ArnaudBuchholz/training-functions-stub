cd4589f008aabc3decd79e51d3c59da62e77db81
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-16 13:20:23 -0400

    Fixed the coverage report to remove the menu

---
+++ scratch.js
@@ -0,0 +1,23 @@
+// Ease the presenter job by offering prepared lines of code to copy & paste
+
+it("is accessible through sinon.spy");
+it("accepts no parameter");
+it("accepts a function parameter");
+it("rejects any other parameter");
+it("returns a function");
+it("returns a function that behaves like the parameter");
+it("returns a function that does nothing when no parameter was specified");
+
+describe("US2", function () {
+
+    describe("it exposes the property \"called\" the indicates if the function was called", function () {
+        it("was never called");
+        it("was called once");
+        var COUNT = 10;
+        it("was called " + COUNT + " times - synchronously");
+    });
+
+    describe("it exposes the property \"callCount\" that indicates how often the function was called", function () {
+    });
+
+});
