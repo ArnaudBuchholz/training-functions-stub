ab3e22ba93b04b3dc4e8047a24e0dfabdda43a09
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-04-18 23:20:26 -0400

    Step 1 of US1

---
+++ ref/us1/1/test.js
@@ -0,0 +1,17 @@
+/**
+ * Tests descriptions are written using the following pattern:
+ *
+ *  describe(label, function () {
+ *
+ *      it(testLabel, function () {
+ *          // Test code, use assert function to validate a boolean condition
+ *          assert(true);
+ *      });
+ *
+ *  });
+ */
+
+describe("US1", function () {
+    "use strict";
+
+});
