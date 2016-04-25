431c57e9b57ab850e4e3f16c3c36e2d379b45712
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-25 16:22:32 -0400

    Synchronization now working

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,8 +1,36 @@
 (function () {
     "use strict";
+    /*global xhrGet*/
 
     function _applyUsRef () {
-        alert(window.usRef);
+        var gearbox = document.getElementById("gearbox"),
+            play = document.getElementById("play");
+        play.className = "hidden";
+        gearbox.className = "";
+        function show () {
+            play.className = "";
+            gearbox.className = "hidden";
+        }
+        xhrGet("/copy?" + window.usRef)
+            .then(function (copyAnswer) {
+                if ("false" === copyAnswer) {
+                    show();
+                    alert("Copy failed");
+                }
+                function wait () {
+                    setTimeout(function () {
+                        xhrGet("/info")
+                            .then(function (infoAnswer) {
+                                var info = JSON.parse(infoAnswer);
+                                if (info.pending) {
+                                    return wait();
+                                }
+                                show();
+                            });
+                    }, 500); // Don't stress the 'server'
+                }
+                wait();
+            });
     }
 
     window.addEventListener("load", function () {
